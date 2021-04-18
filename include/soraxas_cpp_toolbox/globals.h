01d821c08d9f7d9aaf851624fad9615d18f6c7d0
Tin Lai <oscar@tinyiu.com>
2021-04-18 17:39:50 +1000

    implements stats across threads
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- include/soraxas_cpp_toolbox/globals.h
+++ include/soraxas_cpp_toolbox/globals.h
@@ -6,20 +6,59 @@
 #define SXS_GLOBALS
 
 #include "main.h"
+#include <thread>
 #include <variant>
 
 namespace sxs {
 namespace globals {
 
-extern Stats stats;
-extern std::map<std::string, std::shared_ptr<void>> storage;
-// inline static Stats stats = Stats();
-// inline static std::map<std::string, std::shared_ptr<void>> storage;
+namespace {
+// static storage of arbitary things
+std::map<std::string, std::shared_ptr<void>> &_get_static_storage() {
+  static std::map<std::string, std::shared_ptr<void>> storage{};
+  return storage;
+}
 
-#ifdef SXS_GLOBALS_SETUP_STORAGE
-sxs::Stats stats = sxs::Stats();
-std::map<std::string, std::shared_ptr<void>> storage;
-#endif
+// static storage across thread
+static std::mutex &_get_lock() {
+  static std::mutex thread_lock;
+  return thread_lock;
+}
+
+static std::map<std::thread::id, Stats> &_get_stats_container() {
+  static std::map<std::thread::id, Stats> stats_container;
+  return stats_container;
+}
+
+static void _allocate_stats_for_this_thread(std::thread::id thread_id) {
+  auto &thread_lock = _get_lock();
+  thread_lock.lock();
+  _get_stats_container()[thread_id] = Stats{};
+  thread_lock.unlock();
+}
+} // unnamed namespace
+
+static Stats &get_stats() {
+  auto &container = _get_stats_container();
+  auto thread_id = std::this_thread::get_id();
+  auto container_iterator = container.find(thread_id);
+
+  if (container_iterator == container.end()) { /* Not found */
+    _allocate_stats_for_this_thread(thread_id);
+    container_iterator = container.find(thread_id); // need to search again
+    assert(container_iterator != container.end());
+  }
+  return (*container_iterator).second;
+}
+
+static void print_all_stored_stats() {
+  std::cout << "========= StoredStats =========" << std::endl;
+  for (auto &&item : _get_stats_container()) {
+    std::cout << "Thread id: " << item.first << std::endl;
+    std::cout << std::string(item.second) << std::endl;
+  }
+  std::cout << "===============================" << std::endl;
+}
 
 } // namespace globals
 } // namespace sxs
@@ -30,11 +69,12 @@ namespace globals {
 template <typename T> std::shared_ptr<T> create(const std::string &key, T arg) {
   // create a shared ptr object in storage, then return the ptr
   std::shared_ptr<T> ptr = std::make_shared<T>(std::move(arg));
-  storage[key] = ptr;
+  _get_static_storage()[key] = ptr;
   return ptr;
 }
 
 template <typename T> std::shared_ptr<T> get_ptr(const std::string &key) {
+  auto &storage = _get_static_storage();
   auto iter = storage.find(key);
   if (iter == storage.end()) {
     // not found
