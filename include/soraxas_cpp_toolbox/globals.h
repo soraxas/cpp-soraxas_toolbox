d369f9a032fc56e7098862046af7c60969e92f50
Tin Lai <oscar@tinyiu.com>
2021-05-12 00:57:22 +1000

    better handling of global storage
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- include/soraxas_cpp_toolbox/globals.h
+++ include/soraxas_cpp_toolbox/globals.h
@@ -5,33 +5,46 @@
 #ifndef SXS_GLOBALS
 #define SXS_GLOBALS
 
+#include "external/pprint.hpp"
 #include "main.h"
 #include "stats.h"
+#include <map>
 #include <thread>
-#include <variant>
+
+#if __cplusplus >= 201703L
+#include <any>
+namespace sxs {
+namespace globals {
+using std::any;
+using std::any_cast;
+} // namespace globals
+} // namespace sxs
+#else
+#include "soraxas_cpp_toolbox/external/any.hpp"
+namespace sxs {
+namespace globals {
+using nonstd::any;
+using nonstd::any_cast;
+} // namespace globals
+} // namespace sxs
+#endif
 
 namespace sxs {
 namespace globals {
-
 namespace {
-// static storage of arbitary things
-std::map<std::string, std::shared_ptr<void>> &_get_static_storage() {
-  static std::map<std::string, std::shared_ptr<void>> storage{};
-  return storage;
-}
 
 // static storage across thread
-static std::mutex &_get_lock() {
+inline static std::mutex &_get_lock() {
   static std::mutex thread_lock;
   return thread_lock;
 }
 
-static std::map<std::thread::id, Stats> &_get_stats_container() {
+inline static std::map<std::thread::id, Stats> &_get_stats_container() {
   static std::map<std::thread::id, Stats> stats_container;
   return stats_container;
 }
 
-static void _allocate_stats_for_this_thread(std::thread::id thread_id) {
+inline static void _allocate_stats_for_this_thread(std::thread::id thread_id) {
   auto &thread_lock = _get_lock();
   thread_lock.lock();
   _get_stats_container()[thread_id].reset(); // init
@@ -39,9 +52,8 @@ static void _allocate_stats_for_this_thread(std::thread::id thread_id) {
 }
 } // unnamed namespace
 
-static Stats &get_stats() {
+inline static Stats &get_stats(const std::thread::id &thread_id) {
   auto &container = _get_stats_container();
-  auto thread_id = std::this_thread::get_id();
   auto container_iterator = container.find(thread_id);
 
   if (container_iterator == container.end()) { /* Not found */
@@ -52,36 +64,75 @@ static Stats &get_stats() {
   return (*container_iterator).second;
 }
 
-static void print_all_stored_stats() {
+inline static Stats &get_stats() {
+  return get_stats(std::this_thread::get_id());
+}
+
+inline static void print_all_stored_stats() {
   std::cout << "========= StoredStats =========" << std::endl;
+  pprint::PrettyPrinter printer;
   for (auto &&item : _get_stats_container()) {
-    std::cout << "Thread id: " << item.first << std::endl;
-    std::cout << std::string(item.second) << std::endl;
+    //    printer("Thread id: ");
+    printer.print("Thread id: ", item.first);
+    printer.print(std::string(item.second));
   }
   std::cout << "===============================" << std::endl;
 }
 
-template <typename T> std::shared_ptr<T> create(const std::string &key, T arg) {
-  // create a shared ptr object in storage, then return the ptr
-  std::shared_ptr<T> ptr = std::make_shared<T>(std::move(arg));
-  _get_static_storage()[key] = ptr;
-  return ptr;
+///////////////////////////////////////////////////////
+
+struct GlobalStorageKeyNotExists : public std::exception {
+
+  GlobalStorageKeyNotExists(const std::string &key)
+      : std::exception(), message("Given key '" + key + "' does not exists.") {}
+
+  const char *what() const noexcept override { return message.c_str(); }
+
+private:
+  std::string message;
+};
+
+///////////////////////////////////////////////////////
+
+namespace storage {
+
+inline std::map<std::string, any> &_get_static_storage() {
+  static std::map<std::string, any> container{};
+  return container;
 }
 
-template <typename T> std::shared_ptr<T> get_ptr(const std::string &key) {
+bool has_key(const std::string &key) {
+  auto &storage = _get_static_storage();
+  return storage.find(key) != storage.end();
+}
+
+template <typename T> inline T get(const std::string &key) {
   auto &storage = _get_static_storage();
   auto iter = storage.find(key);
   if (iter == storage.end()) {
-    // not found
-    throw std::runtime_error("Given key '" + key + "' does not exists.");
+    auto exc = sxs::globals::GlobalStorageKeyNotExists(key);
+    std::cout << exc.what() << std::endl;
+    throw exc;
   }
-  // obtain the stored shared ptr as a specified type
-  return std::static_pointer_cast<T>((*iter).second);
+  return any_cast<T>(iter->second);
 }
 
-template <typename T> T &get(const std::string &key) {
-  return *get_ptr<T>(key);
+template <typename T> inline void store(const std::string &key, T obj) {
+  _get_static_storage()[key] = std::move(obj);
 }
+
+inline void clear() { _get_static_storage().clear(); }
+
+inline void print_stored_info() {
+  std::cout << "========== Static Storage ==========" << std::endl;
+  for (auto &&item : _get_static_storage()) {
+    std::cout << item.first << ": [type] = " << item.second.type().name()
+              << std::endl;
+  }
+  std::cout << "====================================" << std::endl;
+}
+
+}; // namespace storage
 } // namespace globals
 
 namespace g = globals; // namespace alias
