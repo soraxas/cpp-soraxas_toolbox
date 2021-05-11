d03c125810eb3ae35b05dd37da8e929300278ba8
Tin Lai <oscar@tinyiu.com>
2021-05-12 01:01:41 +1000

    add header to make it easier to work use cpp17 features inolder compiler options
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

---
+++ include/soraxas_cpp_toolbox/future.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#if __cplusplus >= 201703L
+
+#include <filesystem>
+#include <variant>
+
+#else // < C++17
+
+#include "soraxas_cpp_toolbox/external/filesystem/filesystem.hpp"
+#include "soraxas_cpp_toolbox/external/variant.hpp"
+
+namespace std {
+
+using mpark::get;
+using mpark::variant;
+using mpark::visit;
+
+using namespace ghc;
+
+// for usage in pprint
+template <class...> using void_t = void;
+
+template <class T> constexpr bool is_enum_v = is_enum<T>::value;
+
+template <class T> constexpr bool is_signed_v = is_signed<T>::value;
+
+} // namespace std
+
+#endif
