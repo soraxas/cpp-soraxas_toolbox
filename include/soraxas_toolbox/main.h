217d2267db75c632c7129278a14bc598e52f3290
Tin Lai <oscar@tinyiu.com>
2022-07-21 12:26:09 +1000

    add macro for assert statements
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- include/soraxas_toolbox/main.h
+++ include/soraxas_toolbox/main.h
@@ -11,6 +11,32 @@
 
 #define SXS_USE_PPRINT
 
+// https://stackoverflow.com/questions/3767869/adding-message-to-assert
+#ifdef NDEBUG
+#define ASSERT(...) 0
+#else
+//https://stackoverflow.com/questions/3046889/optional-parameters-with-c-macros
+// The multiple macros that you would need anyway [as per: Crazy Eddie]
+#define ASSERT_0()         0
+#define ASSERT_1(condition)         ASSERT_2(condition, "")
+#define ASSERT_2(condition, message)\
+   (!(condition)) ?\
+      (std::cerr << "Assertion failed: (" << #condition << "), "\
+      << "function " << __FUNCTION__\
+      << ", file " << __FILE__\
+      << ", line " << __LINE__ << "."\
+      << std::endl << message << std::endl, abort(), 0) : 1
+// The interim macro that simply strips the excess and ends up with the required macro
+#define ASSERT_X(x,A,B,FUNC, ...)  FUNC
+
+// The macro that the programmer uses
+#define ASSERT(...)                    ASSERT_X(,##__VA_ARGS__,\
+                                          ASSERT_2(__VA_ARGS__),\
+                                          ASSERT_1(__VA_ARGS__),\
+                                          ASSERT_0(__VA_ARGS__)\
+                                         )
+#endif
+
 // template for printing vector container
 template <typename T>
 std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
